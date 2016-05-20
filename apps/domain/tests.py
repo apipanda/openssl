79a00e49d43d9bc7478a17d22011740328aefee0
IniOluwa <inioluwafageyinbo@gmail.com>
2016-05-20 14:26:41 +0100

    Domain tests updated

--- apps/domain/tests.py
+++ apps/domain/tests.py
@@ -9,64 +9,91 @@ import unittest
 class Domain_Tests(TestCase):
 
     def setUp(self):
-        self.openssl_domain = Domain(
-            domain_name="openssl",
-            domain_url="openssl.io",
-            domain_registerer="Bernard Ojengwa",
-            support_email="bernardojengwa@gmail.com",
-            top_level_domain="google.com",
-            domain_certificate=Certificate.objects.get(id=1),
-            date_registered=date(2016, 1, 1),
-            expiration_date=date(2017, 1, 1),
-            last_updated=date.today() - timedelta(days=183),
-            date_entered=date.today(),
+        self.mock = {
+            "domain_name": "openssl",
+            "domain_url": "openssl.io",
+            "domain_registerer": "Bernard Ojengwa",
+            "support_email": "bernardojengwa@gmail.com",
+            "top_level_domain": "google.com",
+            "domain_certificate": Certificate.objects.latest('last_updated'),
+            "date_registered": date(2016, 1, 1),
+            "expiration_date": date(2017, 1, 1),
+            "last_updated": date.today() - timedelta(days=183),
+            "date_entered": date.today(),
+        }
+        Domain.objects.create(
+            domain_name=self.mock["domain_name"],
+            domain_url=self.mock["openssl.io"],
+            domain_registerer=self.mock["domain_registerer"],
+            support_email=self.mock["support_email"],
+            top_level_domain=self.mock["top_level_domain"],
+            domain_certificate=self.mock["domain_certificate"],
+            date_registered=self.mock["date_registered"],
+            expiration_date=self.mock["expiration_date"],
+            last_updated=self.mock["last_updated"],
+            date_entered=self.mock["date_entered"],
         )
-        self.openssl_domain.save()
 
     def test_domain_exists(self):
-        domain = self.openssl_domain.objects.id(1)
+        domain = Domain.objects.latest('date_entered')
         self.assertIsInstance(domain, int)
 
-    # def test_domain_name_availability(self):
-    #     domain_name = self.openssl_domain.objects.get(name="domain_name")
-    #     self.assertEqual(domain_name, "openssl")
+    def test_domain_name_availability(self):
+        domain = Domain.objects.latest('date_entered')
+        name = domain.domain_name
+        self.assertEquals(name, self.mock["domain_name"])
 
-    # def test_domain_url_specificity(self):
-    #     url = self.openssl_domain.objects.get(name="url")
-    #     self.assertIs(url, "openssl.io")
+    def test_domain_url_specificity(self):
+        domain = Domain.objects.latest('date_entered')
+        url = domain.domain_url
+        self.assertEquals(url, self.mock["domain_url"])
 
-    # def test_support_email_is_an_email(self):
-    #     email = self.openssl_domain.objects.get(name="support_email")
-    #     mail = False
-    #     for char in email:
-    #         if char == "@":
-    #             mail = True
-    #         return mail
-    #     self.assertIs(mail, True)
+    def test_support_email_is_an_email(self):
+        domain = Domain.objects.latest('date_entered')
+        email = domain.support_email
+        mail = False
+        for char in email:
+            if char == "@":
+                mail = True
+            return mail
+        self.assertIs(mail, True)
 
-    # def test_top_level_domain(self):
-    #     tld = self.openssl_domain.objects.get(name="top_level_domain")
-    #     self.assertNotEqual(tld, False)
+    def test_top_level_domain(self):
+        domain = Domain.objects.latest('date_entered')
+        tld = domain.top_level_domain
+        self.assertEquals(tld, self.mock["top_level_domain"])
 
-    # def domain_to_certificate_relationship(self):
-    #     cert = self.openssl_domain.objects.get(pk=1)
-    #     self.assertTrue(cert.openssl_certificate.ssl_key, "thisisakey")
+    def domain_to_certificate_relationship(self):
+        certificate = Certificate.objects.latest('last_updated')
+        self.assertIsInstance(certificate, int)
 
-    # def test_date_registered(self):
-    #     registered = self.openssl_domain.objects.get(name="date_registered")
-    #     self.assertIs(registered, date(2016, 01, 01))
+    def test_date_registered(self):
+        domain = Domain.objects.latest('date_entered')
+        registered = domain.date_registered
+        self.assertEquals(registered, self.mock["date_registered"])
 
-    # def test_expiration(self):
-    #     expiration = self.openssl_domain.objects.get(name="expiration_date")
-    #     self.assertIs(expiration, date(2017, 01, 01))
+    def test_expiration(self):
+        domain = Domain.objects.latest('date_entered')
+        expiration = domain.expiration_date
+        self.assertEquals(expiration, self.mock["expiration_date"])
 
-    # def test_last_updated(self):
-    #     updated = self.openssl_domain.objects.get(name="last_updated")
-    #     self.assertEqual(updated, date.today() - timedelta(days=183))
+    def test_last_updated(self):
+        domain = Domain.objects.latest('date_entered')
+        updated = domain.last_updated
+        self.assertEquals(updated, self.mock["last_updated"])
 
-    # def test_date_entered(self):
-    #     entry = self.openssl_domain.objects.get(name="date_entered")
-    #     self.assertEqual(entry, date.today())
+    def test_date_entered(self):
+        domain = Domain.objects.latest('date_entered')
+        entry = domain
+        self.assertEquals(entry, self.mock["date_entered"])
+
+    def test_url_can_be_updated(self):
+        domain = Domain.objects.latest('date_entered')
+        domain.domain_url = "bernardojengwa.com"
+        domain.save()
+        url = domain.domain_url
+        self.assertIsNotNone(domain.objects.latest('date_entered').url)
+        self.assertEquals(url, "bernardojengwa.com")
 
 if __name__ == '__main__':
     unittest()
