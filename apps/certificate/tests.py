aee91f16995988842123e913d0b57dc87e1cfa4d
IniOluwa <inioluwafageyinbo@gmail.com>
2016-05-20 12:35:11 +0100

    certificate tests refactored

--- apps/certificate/tests.py
+++ apps/certificate/tests.py
@@ -8,45 +8,63 @@ import unittest
 class Certificate_Tests(TestCase):
 
     def setUp(self):
-        self.openssl_certificate = Certificate.objects.create(
-            ssl_certificate="thisisacertificate",
-            ssl_key="thisisakey",
-            domain="openssl.io",
-            date_registered=date.today() + timedelta(days=-10),
-            expiration_date=date.today() + timedelta(days=10),
-            last_updated=date.today() + timedelta(days=-5)
+        self.mock = {
+            "ssl_certificate": "thisisacertificate",
+            "ssl_key": "thisisakey",
+            "domain": "openssl.io",
+            "date_registered": date.today() + timedelta(days=-10),
+            "expiration_date": date.today() + timedelta(days=10),
+            "last_updated": date.today() + timedelta(days=-5),
+        }
+        Certificate.objects.create(
+            ssl_certificate=self.mock["ssl_certificate"],
+            ssl_key=self.mock["ssl_key"],
+            domain=self.mock["domain"],
+            date_registered=self.mock["date_registered"],
+            expiration_date=self.mock["expiration_date"],
+            last_updated=self.mock["last_updated"],
         )
-        self.openssl_certificate.save()
 
     def test_certificate_exists(self):
-        certificate_id = self.openssl_certificate.objects.get(id)
-        self.assertTrue(certificate_id, int)
+        certificate = Certificate.objects.latest('last_updated')
+        self.assertIsInstance(certificate.id, int)
 
     def test_ssl_certificate(self):
-        certificate = self.openssl_certificate.objects.get(
-            name="ssl_certificate")
-        self.assertTrue(certificate, str)
+        certificate = Certificate.objects.latest('date_registered')
+        self.assertEquals(
+            certificate.ssl_certificate, self.mock["ssl_certificate"])
+
+    def test_certificate_can_be_updated(self):
+        certificate = Certificate.objects.latest('last_updated')
+        certificate.ssl_certificate = "Got a new cert"
+        certificate.save()
+        self.assertIsNotNone(Certificate.objects.filter(
+            ssl_certificate="Got a new certificate"))
 
     def test_ssl_key(self):
-        certificate_key = self.openssl_certificate.objects.get(name="ssl_key")
-        self.assertTrue(certificate_key, str)
+        certificate = Certificate.objects.latest('last_updated')
+        certificate_key = certificate.ssl_key
+        self.assertIsNotNone(certificate_key)
 
     def test_domain_exists(self):
-        domain = self.openssl_certificate.objects.get(name="domain")
-        self.assertEqual(domain, "openssl.io")
+        certificate = Certificate.objects.latest('last_updated')
+        certificate_domain = certificate.domain
+        self.assertEquals(certificate_domain, "openssl.io")
 
-    def test_date_resigtered(self):
-        registered = self.openssl_certificate.objects.get(
-            name="date_registered")
-        self.assertIs(registered, date(2016, 04, 14))
+    def test_date_registered(self):
+        certificate = Certificate.objects.latest('last_updated')
+        registered = certificate.date_registered
+        self.assertEquals(registered, self.mock["date_registered"])
 
     def test_expiration_date(self):
-        expiring = self.openssl_certificate.objects.get(name="expiration")
-        self.asserIs(expiring, date(2017, 04, 14))
+        certificate = Certificate.objects.latest('last_updated')
+        expiring = certificate.expiration_date
+        self.assertEquals(expiring, self.mock["expiration_date"])
 
     def test_last_updated(self):
-        updated = self.openssl_certificate.objects.get(name="last_updated")
-        self.assertEqual(updated, date.today())
+        certificate = Certificate.objects.latest('last_updated')
+        updated = certificate.last_updated
+        self.assertEquals(updated, self.mock["last_updated"])
 
 if __name__ == '__main__':
     unittest()
