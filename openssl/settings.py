d3b184bc5c68bf7bc40a412d622b7b7b8d21200e
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-05-29 11:08:48 +0100

    update settings module
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- openssl/settings.py
+++ openssl/settings.py
@@ -131,3 +131,46 @@ NOSE_ARGS = [
     '--with-coverage',
     '--cover-package=apps.api',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'slack': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'slack.utils.SlackHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'slack'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+IS_SLACK_ENABLED = True
+SLACK_TOKEN = '<token>'
+SLACK_CHANNEL = '#<channel>'
+SLACK_USERNAME = '<username>'
+
+SLACK_PARAMS = {
+    'GET': True,
+    'POST': True,
+    'META': {
+        'SERVER_NAME': True,
+        'HTTP_ACCEPT': True
+    }
+}
