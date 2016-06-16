1c7a2add95d4531e89b06dd299c577af263fd6e9
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-16 03:14:26 +0100

    update domain model
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- apps/domain/models.py
+++ apps/domain/models.py
@@ -9,20 +9,20 @@ class Domain(models.Model):
     domain_name = models.CharField(max_length=200)
     domain_url = models.CharField(max_length=200)
     domain_registerer = models.CharField(max_length=200)
-    support_email = models.CharField(max_length=200)
+    support_email = models.EmailField(max_length=200)
     tld = models.CharField(max_length=200)
-    domain_certificate = models.OneToOneField(
-        Certificate, related_name='certified_domain')
     slug = models.SlugField(max_length=100)
     date_registered = models.DateField()
     expiration_date = models.DateField()
-    last_updated = models.DateField()
+    last_updated = models.DateField(auto_now=True)
     date_entered = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+    domain_certificate = models.ForeignKey(
+        Certificate, related_name='certified_domain')
     admin = models.ForeignKey(User, related_name='domains')
 
     def get_webmaster(self):
-        self.admin
+        return self.admin
 
     @property
     def is_expired(self):
@@ -30,5 +30,5 @@ class Domain(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = self.domain_name + self.tld.replace('.', '-')
+            self.slug = (self.domain_name + self.tld).replace('.', '-')
         return super(Domain, self).save(*args, **kwargs)
