b58cb616a35264488bb3f0561df63fecac426375
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-18 17:59:34 +0100

    update routes
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/config/routes.js
+++ public/app/config/routes.js
@@ -7,77 +7,74 @@ app.config(["$routeProvider", '$locationProvider', function ($routeProvider, $lo
         controllerAs: 'vm',
         templateUrl: '../home/'
     })
-        .when('/hubs', {
-            controller: 'HubsController',
-            controllerAs: 'vm',
-            templateUrl: '../hubs/'
-        })
-        .when('/login', {
-            controller: 'LoginController',
-            controllerAs: 'vm',
-            templateUrl: '../login/'
-        })
-        .when('/signup', {
-            controller: 'RegisterController',
-            controllerAs: 'vm',
-            templateUrl: '../register/'
-        })
-        .when('/reset', {
-            controller: 'ResetController',
-            controllerAs: 'vm',
-            templateUrl: '../reset/'
-        })
+        // .when('/hubs', {
+        //     controller: 'HubsController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../hubs/'
+        // })
+        // .when('/login', {
+        //     controller: 'LoginController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../login/'
+        // })
+        // .when('/signup', {
+        //     controller: 'RegisterController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../register/'
+        // })
+        // .when('/reset', {
+        //     controller: 'ResetController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../reset/'
+        // })
 
-        .whenAuthenticated('/switcher', {
-            controller: 'SwitchController',
-            controllerAs: 'vm',
-            templateUrl: '../switcher/'
-        })
+        // .whenAuthenticated('/switcher', {
+        //     controller: 'SwitchController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../switcher/'
+        // })
 
-        .whenAuthenticated('/dashboard', {
-            controller: 'DashController',
-            controllerAs: 'vm',
-            templateUrl: '../dashboard/'
-        })
+        // .whenAuthenticated('/dashboard', {
+        //     controller: 'DashController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../dashboard/'
+        // })
 
-        .whenAuthenticated('/dashboard/hubs', {
-            controller: 'HubController',
-            controllerAs: 'vm',
-            templateUrl: '../hub/'
-        })
+        // .whenAuthenticated('/dashboard/hubs', {
+        //     controller: 'HubController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../hub/'
+        // })
 
-        .whenAuthenticated('/dashboard/orgs', {
-            controller: 'OrgController',
-            controllerAs: 'vm',
-            templateUrl: '../orgs/'
-        })
+        // .whenAuthenticated('/dashboard/orgs', {
+        //     controller: 'OrgController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../orgs/'
+        // })
 
-        .whenAuthenticated('/dashboard/workspaces', {
-            controller: 'WorkspaceController',
-            controllerAs: 'vm',
-            templateUrl: '../workspaces/'
-        })
+        // .whenAuthenticated('/dashboard/workspaces', {
+        //     controller: 'WorkspaceController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../workspaces/'
+        // })
 
-        .whenAuthenticated('/dashboard/plugins', {
-            controller: 'PluginController',
-            controllerAs: 'vm',
-            templateUrl: '../plugins/'
-        })
+        // .whenAuthenticated('/dashboard/plugins', {
+        //     controller: 'PluginController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../plugins/'
+        // })
 
-        .whenAuthenticated('/profile', {
-            controller: 'ProfileController',
-            controllerAs: 'vm',
-            templateUrl: '../profile/'
-        })
+        // .whenAuthenticated('/profile', {
+        //     controller: 'ProfileController',
+        //     controllerAs: 'vm',
+        //     templateUrl: '../profile/'
+        // })
 
         .otherwise({redirectTo: '/'});
 }])
     .config(['$httpProvider', function ($httpProvider) {
         "use strict";
         $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-    }])
-    .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-        cfpLoadingBarProvider.includeSpinner = false;
     }])
     .config(['$interpolateProvider', function ($interpolateProvider) {
         "use strict";
@@ -86,5 +83,6 @@ app.config(["$routeProvider", '$locationProvider', function ($routeProvider, $lo
     }])
     .config(['$localStorageProvider', function ($localStorageProvider) {
         "use strict";
-        $localStorageProvider.setKeyPrefix('panda');
+        $localStorageProvider.setKeyPrefix('pandassl');
     }]);
+
