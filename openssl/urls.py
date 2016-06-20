fd86f383207b0998c54010806635dd69c1e48b1e
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-20 01:21:09 +0100

    update url conf
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- openssl/urls.py
+++ openssl/urls.py
@@ -32,6 +32,7 @@ api.register(resource.CertificateResource())
 urlpatterns = (
     url(r'^api/', include(api.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^app/', TemplateView.as_view(template_name='map.html')),
+
+    url(r'^home/', TemplateView.as_view(template_name='common/home.html')),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
 )
