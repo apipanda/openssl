68d9b74da53caa30ff0d5aa6cbc579e06ec65a41
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-22 01:59:50 +0100

    update wsgi module
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- openssl/wsgi.py
+++ openssl/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openssl.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openssl.settings.prod")
 
 application = get_wsgi_application()
