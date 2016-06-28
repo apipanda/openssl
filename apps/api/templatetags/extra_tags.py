1729e3c61f120f2c7163e3bb0b585c7ba4302006
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-28 02:01:41 +0100

    update custom json tags
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- apps/api/templatetags/extra_tags.py
+++ apps/api/templatetags/extra_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.core import serializers
 from django.utils.safestring import mark_safe
+from django.utils.html import escapejs
 
 
 register = template.Library()
@@ -8,7 +9,8 @@ register = template.Library()
 
 @register.filter(name='json', needs_autoescape=True)
 def to_json(data, autoescape=True):
+    data = data.select_related('author__username')
     dump = serializers.serialize(
-        'json', data, fields=['title', 'slug', 'author', 'markup', 'section', ])
-
-    return mark_safe(dump)
+        'json', data)
+    # print(data.values())
+    return mark_safe(escapejs(dump))
