5191fe73df4f238a208b654ae5de0157a9affe9e
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-29 00:21:46 +0100

    update url conf
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- openssl/urls.py
+++ openssl/urls.py
@@ -1,4 +1,4 @@
-"""mapoint URL Configuration.
+"""openssl URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
