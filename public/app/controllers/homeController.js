8219878882007790cdaa14c28e660590b25bf496
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-06-30 02:03:11 +0100

    update hoe controller
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -4,17 +4,23 @@ app.controller("HomeController", [
     "$log",
     "Request",
     "domainBase",
-    "verifyUrl",
-    function ($scope, $location, $log, Request, domainBase, verifyUrl) {
+    "whoisUrl",
+    function ($scope, $location, $log, Request, domainBase, whoisUrl) {
         'use strict';
-
+        $log.debug(console.dir(Request.fetch));
         $scope.verify = function () {
-            var requestUrl = domainBase + verifyUrl;
-            var data = $scope.domain;
+            console.log($scope.domain);
+            var domain = ($.url($scope.domain)).attr();
+            var requestUrl = domainBase + whoisUrl;
+            var data = domain;
             $scope.disableBtn = true;
             Request.fetch(requestUrl, data)
-                .then(function (status, response, headers) {
+                .then(function (response) {
                     // body...
+                    console.log(response);
+                }, function (error) {
+                    // body...
+                    console.log(error);
                 });
 
         };
