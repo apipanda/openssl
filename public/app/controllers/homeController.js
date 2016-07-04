1c165d26e2871bdd6b3ce02e15f0e52a84289f6b
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-07-05 00:01:51 +0100

    update home controller
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -8,33 +8,40 @@ app.controller("HomeController", [
     "whoisUrl",
     function ($scope, $location, $log, $localStorage, Request, domainBase, whoisUrl) {
         'use strict';
+        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(expression);
+
         $scope.whois = function () {
             $scope.error = null;
             $scope.warn = null;
             $scope.status = null;
             $scope.disableBtn = true;
-            var data = ($.url($scope.domain)).attr();
-            var requestUrl = domainBase + whoisUrl;
-            // console.log($scope.domain, data);
-            Request.fetch(requestUrl, data)
-                .then(function (response) {
-                    // body...
-                    if (!response.success) {
-                        $scope.warn = response.message;
-                        $scope.status = response.status + ' -';
-                    }
-                    $localStorage.domain = response.data;
+            if (!!!$scope.domain.match(regex)) {
+                $scope.error = "Your input is not a valid domain name.";
+                $scope.status = 400;
+            } else {
+                var data = ($.url($scope.domain)).attr();
+                var requestUrl = domainBase + whoisUrl;
+                // console.log($scope.domain, data);
+                Request.fetch(requestUrl, data)
+                    .then(function (response) {
+                        // body...
+                        if (!response.success) {
+                            $scope.warn = response.message;
+                            $scope.status = response.status + ' -';
+                        }
+                        $localStorage.domain = response.data;
 
-                    $location.path('/verify');
+                        $location.path('/verify');
 
-                }, function (error) {
-                    // body...
-                    $scope.error = error.statusText;
-                    $scope.status = error.status + ' -';
-                });
+                    }, function (error) {
+                        // body...
+                        $scope.error = error.statusText;
+                        $scope.status = error.status + ' -';
+                    });
+            }
+
             $scope.disableBtn = false;
 
         };
-
-        $log.info("Home Controller Initialized");
     }]);
