c4d0e12ac69962ea3711625f531abf9696a55182
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-07-04 21:32:42 +0100

    updat verification controller/
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/verifyController.js
+++ public/app/controllers/verifyController.js
@@ -2,8 +2,44 @@ app.controller("VerifyController", [
     "$scope",
     "$location",
     "$log",
-    function ($scope, $location, $log) {
+    "$localStorage",
+    "Request",
+    "domainBase",
+    "whoisUrl",
+    "verifyUrl",
+    function ($scope, $location, $log, $localStorage, Request, domainBase, whoisUrl, verifyUrl) {
         'use strict';
+        if (!!!$localStorage.domain) {
+            $location.path('/');
+        }
 
-        $log.debug("Hubs Controller Initialized");
+        $scope.whois = function () {
+            $scope.error = null;
+            $scope.warn = null;
+            $scope.status = null;
+            $scope.disableBtn = true;
+            var data = ($.url($scope.domain)).attr();
+            var requestUrl = domainBase + whoisUrl;
+            // console.log($scope.domain, data);
+            Request.fetch(requestUrl, data)
+                .then(function (response) {
+                    // body...
+                    if (!response.success) {
+                        $scope.warn = response.message;
+                        $scope.status = response.status + ' -';
+                    }
+                    $localStorage.domain = response.data;
+
+                    $location.path('/verify');
+
+                }, function (error) {
+                    // body...
+                    $scope.error = error.statusText;
+                    $scope.status = error.status + ' -';
+                });
+            $scope.disableBtn = false;
+
+        };
+
+        $log.debug("Verify Controller Initialized");
     }]);
