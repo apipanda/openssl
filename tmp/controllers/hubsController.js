f3730555694f441c54046274f449cea635c79baf
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 22:54:49 +0100

    Update tmp/controllers/hubsController.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- tmp/controllers/hubsController.js
+++
@@ -1,9 +0,0 @@
-app.controller("HubsController", [
-    "$scope",
-    "$location",
-    "$log",
-    function ($scope, $location, $log) {
-        'use strict';
-
-        $log.debug("Hubs Controller Initialized");
-    }]);
