bfefa1b73eeda8b6b1d52124875c6a929959ac32
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 23:59:04 +0100

    Update app/views/main/forms.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- app/views/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm as Form
-from wtforms import StringField
-from wtforms import SubmitField
-from wtforms.validators import Required
-
-
-class SearchForm(Form):
-    bvn = StringField("Enrollees BVN", validators=[Required()])
-    submit = SubmitField('Submit')
