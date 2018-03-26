89f194af6a5a4e46afffee480b97a3ac013e29d5
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 22:43:18 +0100

    Update public/app/controllers/account/editController.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/account/editController.js
+++
@@ -1,9 +0,0 @@
-app.controller("EditController", [
-    "$scope",
-    "$location",
-    "$log",
-    function ($scope, $location, $log) {
-        'use strict';
-
-        $log.debug("Plugins Controller Initialized");
-    }]);
