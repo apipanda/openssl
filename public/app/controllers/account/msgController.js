35972a7829d56ae9f3a986f555ffe7187421e41c
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 22:43:18 +0100

    Update public/app/controllers/account/msgController.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/account/msgController.js
+++
@@ -1,9 +0,0 @@
-app.controller("MessageController", [
-    "$scope",
-    "$location",
-    "$log",
-    function ($scope, $location, $log) {
-        'use strict';
-
-        $log.debug("Org Controller Initialized");
-    }]);
