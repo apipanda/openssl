38a814bc50f863f1fd266a04c747fc9490126084
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 22:43:19 +0100

    Update public/app/controllers/guideController.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/guideController.js
+++
@@ -1,9 +0,0 @@
-app.controller("GuideController", [
-    "$scope",
-    "$location",
-    "$log",
-    function ($scope, $location, $log) {
-        'use strict';
-
-        $log.debug("Hubs Controller Initialized");
-    }]);
