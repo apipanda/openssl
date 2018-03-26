61517963e94f09ee0cf69b1259e0c74a3190f436
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 22:43:18 +0100

    Update public/app/controllers/account/profileController.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/app/controllers/account/profileController.js
+++
@@ -1,9 +0,0 @@
-app.controller("ProfileController", [
-    "$scope",
-    "$location",
-    "$log",
-    function ($scope, $location, $log) {
-        'use strict';
-
-        $log.debug("Profile Controller Initialized");
-    }]);
