89ce0867011b7604908b234d35282398acbf3cef
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-26 22:54:47 +0100

    Update tmp/controllers/account/logsController.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- tmp/controllers/account/logsController.js
+++
@@ -1,9 +0,0 @@
-app.controller("LogController", [
-    "$scope",
-    "$location",
-    "$log",
-    function ($scope, $location, $log) {
-        'use strict';
-
-        $log.debug("Org Controller Initialized");
-    }]);
